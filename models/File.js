a706efbd1f3727a5d4e7b090f4a2f84eea30977b
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-28 17:53:17 +0530

    Episode - 11 - File Upload - nodemailer install, after db call post middleware created transported created and call crediential and send function call

--- models/File.js
+++ models/File.js
@@ -16,5 +16,37 @@ const fileSchema = new mongoose.Schema({
         type: String
     }
 });
+
+//Post middleware
+fileSchema.post("save", async function (doc) {
+    try {
+        console.log("doc", doc);
+
+        //define transporter
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        });
+
+        //mail send
+        let info = await transporter.sendMail({
+            from: 'Codehelp',
+            to: doc.email,
+            subject: "New file upload on cloudinary",
+            html: `<h1>Hello Jee,</h1> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a></p> `
+        });
+
+        console.log("Info: ", info);
+
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+
+
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
