4d54266199eef265a815886e24d2e6fed2ff91be
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-28 15:46:38 +0530

    Episode - 11 - File Upload - videoUpload function - routes done

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -82,3 +82,54 @@ exports.imageUpload = async (req, res) => {
         })
     }
 }
+
+//video upload ka handler
+exports.videoUpload = async (req,res) => {
+    try{
+        //data fetch
+        const { name, tags, email} = req.body;
+        console.log(name,tags,email);
+        
+        const file = req.files.videoFile;
+
+         //Validation
+         const supportedTypes = ["mp4", "mkv"];
+         const fileType = file.name.split('.')[1].toLowerCase();
+         console.log("File Type:", fileType);
+ 
+         //TODO: add a upper limit of 5MB for Video
+         if(!isFileTypeSupported(fileType, supportedTypes)) {
+             return res.status(400).json({
+                 success:false,
+                 message:'File format not supported',
+             })
+         }
+
+          //file format supported hai
+        console.log("Uploading to Codehelp");
+        const response = await uploadFileToCloudinary(file, "Codehelp");
+        console.log(response);
+
+        //db me entry save krni h
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl:response.secure_url,
+        });
+
+        res.json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:'Video Successfully Uploaded',
+        })
+
+    }
+    catch(error) {
+        console.error(error);
+        res.status(400).json({
+            success:false,
+            message:'Something went wrong',
+        })
+    }
+}
